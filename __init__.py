6ff7abafbc4e7bddd6e654308add81dcc9c06be1
moagstar <moagstar@gmail.com>
2016-04-28 21:03:38 +0200

    fixed up the links to leet code

--- __init__.py
+++ __init__.py
@@ -4,10 +4,27 @@ from IPython.core.display import display, HTML
 
 
 def leet_puzzle(problem):
-    response = get('https://leetcode.com/problems/' + problem)
-    soup = BeautifulSoup(response.content)
-    content = str(soup.select('.question-content')[0])
+    
     html = '<a href="https://leetcode.com/problems/' + problem
     html += '">Source : https://leetcode.com/problems/' + problem + '</a>'
-    html += content
+    
+    response = get('https://leetcode.com/problems/' + problem)
+    soup = BeautifulSoup(response.content)
+    content = soup.select('.question-content')[0]
+    
+    for a in content.select('a'):
+        if a['href'] == '/subscribe/':
+            a.nextSibling.extract()
+            a.extract()
+        else:
+            a['href'] = 'https://leetcode.com' + a['href']
+
+    for tags in content.select('#tags'):
+        tags.extract()
+        
+    for similar in content.select('#similar'):
+        similar.extract()
+        
+    html += str(content)
+    
     display(HTML(html))
